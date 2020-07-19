3d58a3095c9d5f89770592ebe8e916d7e9f65c16
huayingcao2 <huayingcao2@jd.com>
2020-07-19 11:57:23 +0800

    init projects

---
+++ accumulate-coding-leetcode/src/main/java/com/accumuate/coding/leetcode/AccumulateCodingLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.accumuate.coding.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccumulateCodingLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccumulateCodingLeetcodeApplication.class, args);
+    }
+
+}
