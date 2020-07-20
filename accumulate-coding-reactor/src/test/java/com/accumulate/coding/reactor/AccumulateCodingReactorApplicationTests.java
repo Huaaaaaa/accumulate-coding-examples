af92f0a61cb8d7668d6a57cfe155d8d5283a03fe
huayingcao2 <huayingcao2@jd.com>
2020-07-20 09:44:19 +0800

    add module

---
+++ accumulate-coding-reactor/src/test/java/com/accumulate/coding/reactor/AccumulateCodingReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accumulate.coding.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccumulateCodingReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
