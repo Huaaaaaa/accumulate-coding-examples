0a79914ed8f7227d3ba36325d54163d31bc204de
Huaaaaaa <huayingcao2@jd.com>
2020-12-05 23:55:45 +0800

    Single Responsibility Principle

---
+++ accumulate-coding-design/src/main/java/com/accumulate/coding/design/principle/srpagainst/SrpAgainstOrderIPay.java
@@ -0,0 +1,15 @@
+package com.accumulate.coding.design.principle.srpagainst;
+
+/**
+ * Author: Huaaaaaa
+ * Date:  2020/12/5
+ * Todo:
+ * Time 23:47
+ */
+public class SrpAgainstOrderIPay implements SrpAgainstIPay {
+
+    @Override
+    public String pay(String payMethod) {
+        return payMethod;
+    }
+}
