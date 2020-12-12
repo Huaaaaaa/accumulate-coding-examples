3af54719d0d23504c83c550e632657d6ba921ffe
Huaaaaaa <huayingcao2@jd.com>
2020-12-12 18:45:09 +0800

    RandomUtil

---
+++ accumulate-coding-common/src/main/java/com/accumulate/coding/common/utils/RandomUtil.java
@@ -0,0 +1,17 @@
+package com.accumulate.coding.common.utils;
+
+import java.util.UUID;
+
+/**
+ * Author: Huaaaaaa
+ * Date:  2020/12/12
+ * Todo:
+ * Time 15:06
+ */
+public class RandomUtil {
+
+    public static String getUUID() {
+        String pId = UUID.randomUUID().toString();
+        return pId.toUpperCase();
+    }
+}
